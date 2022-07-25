15eea0b152d7346448ace5e81138fc0738be840a
Jowey-jose <josephmbuku17@gmail.com>
2022-07-25 20:03:22 +0300

    first commit

---
+++ src/main/java/com/armory/armorymanagementbackend/ArmoryManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.armory.armorymanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmoryManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArmoryManagementBackendApplication.class, args);
+    }
+
+}
